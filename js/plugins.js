bdbdcebbeef35fe88062875f3dc49d20fd072c4e
lisachouHC <105439296+lisachouHC@users.noreply.github.com>
2023-12-09 12:45:47 +0800

    CWT3.1 版
    刪掉片頭動畫！

--- js/plugins.js
+++ js/plugins.js
@@ -3,7 +3,7 @@
 var $plugins =
 [
 {"name":"Community_Basic","status":true,"description":"Basic plugin for manipulating important parameters.","parameters":{"cacheLimit":"20","screenWidth":"816","screenHeight":"624","changeWindowWidthTo":"","changeWindowHeightTo":"","renderingMode":"auto","alwaysDash":"off"}},
-{"name":"MadeWithMv","status":true,"description":"Show a Splash Screen \"Made with MV\" and/or a Custom Splash Screen before going to main screen.","parameters":{"Show Made With MV":"true","Made with MV Image":"MadeWithMv","Show Custom Splash":"false","Custom Image":"","Fade Out Time":"120","Fade In Time":"120","Wait Time":"160"}},
+{"name":"MadeWithMv","status":false,"description":"Show a Splash Screen \"Made with MV\" and/or a Custom Splash Screen before going to main screen.","parameters":{"Show Made With MV":"true","Made with MV Image":"MadeWithMv","Show Custom Splash":"false","Custom Image":"","Fade Out Time":"120","Fade In Time":"120","Wait Time":"160"}},
 {"name":"Chimaki_Shake","status":true,"description":"提供上下震動功能","parameters":{}},
 {"name":"DTextPicture","status":true,"description":"[ ver.1.15.0 ]產生動態文字圖片","parameters":{"itemIconSwitchId":"0"}}
 ];
